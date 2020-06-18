edd03be1fb7633239b09bc555c9270dfb1adba16
Quentin Bertrand <quentin.bertrand@yahoo.fr>
2020-06-18 18:23:04 +0200

    Fixed selenium test

--- tests/integration/tests.py
+++ tests/integration/tests.py
@@ -2,6 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+
+from users.models import User
 
 
 chrome_options = Options()
@@ -9,18 +12,19 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument('window-size=1920x1080')
 
 
-class AccountTestCase(LiveServerTestCase):
+class AccountTestCase(StaticLiveServerTestCase):
     def setUp(self) -> None:
         self.selenium = webdriver.Chrome(options=chrome_options)
         self.selenium.implicitly_wait(5)
         super(AccountTestCase, self).setUp()
+        User.objects.create_user(email="test@test.com", password="Test#2020")
 
     def tearDown(self) -> None:
         self.selenium.quit()
         super(AccountTestCase, self).tearDown()
 
     def test_existing_user_can_login(self):
-        self.selenium.get("http://127.0.0.1:8000/users/login")
+        self.selenium.get(f"{self.live_server_url}/users/login")
 
         email = self.selenium.find_element_by_id("id_email")
         password = self.selenium.find_element_by_id("id_password")
@@ -28,7 +32,7 @@ class AccountTestCase(LiveServerTestCase):
         self.assertIn("Se connecter", self.selenium.title)
 
         email.send_keys("test@test.com")
-        password.send_keys("test")
+        password.send_keys("Test#2020")
 
         submit_button = self.selenium.find_element_by_css_selector("input.btn-light")
         submit_button.click()
